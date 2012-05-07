daa07961f5020bfbbc90589ba64d61015f9e5023
Antew <antewcode@gmail.com>
2012-05-07 18:53:26 -0400

    Fix project properties, add Main class.

---
+++ d3_android/src/com/pate/diablo/Main.java
@@ -0,0 +1,18 @@
+package com.pate.diablo;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+
+public class Main extends SherlockActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
