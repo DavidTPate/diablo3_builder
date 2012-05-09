c6f90f3a9b570cec83ab4e8eea6cf3ad4e126213
Antew <antewcode@gmail.com>
2012-05-08 23:11:50 -0400

    Add basic rune layout.

--- d3_android/src/com/pate/diablo/sectionlist/EntryAdapter.java
+++ d3_android/src/com/pate/diablo/sectionlist/EntryAdapter.java
@@ -13,52 +13,55 @@ import android.widget.TextView;
 import com.pate.diablo.R;
 import com.pate.diablo.model.Skill;
 
-public class EntryAdapter extends ArrayAdapter<Item> {
+public class EntryAdapter extends ArrayAdapter<Item>
+{
 
-	private Context context;
-	private ArrayList<Item> items;
-	private LayoutInflater vi;
+    private Context         context;
+    private ArrayList<Item> items;
+    private LayoutInflater  vi;
 
-	public EntryAdapter(Context context,ArrayList<Item> items) {
-		super(context,0, items);
-		this.context = context;
-		this.items = items;
-		vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	}
+    public EntryAdapter(Context context, ArrayList<Item> items) {
+        super(context, 0, items);
+        this.context = context;
+        this.items = items;
+        vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View v = convertView;
+        final Item i = items.get(position);
+        if (i != null) {
+            if (i.isSection()) 
+            {
+                SectionItem si = (SectionItem) i;
+                v = vi.inflate(R.layout.list_item_section, null);
 
-		final Item i = items.get(position);
-		if (i != null) {
-			if(i.isSection()){
-				SectionItem si = (SectionItem)i;
-				v = vi.inflate(R.layout.list_item_section, null);
+                v.setOnClickListener(null);
+                v.setOnLongClickListener(null);
+                v.setLongClickable(false);
 
-				v.setOnClickListener(null);
-				v.setOnLongClickListener(null);
-				v.setLongClickable(false);
-				
-				final TextView sectionView = (TextView) v.findViewById(R.id.list_item_section_text);
-				sectionView.setText(si.getTitle());
-			}else{
-			    Skill s = ((EntryItem) i).getSkill();
-				v = vi.inflate(R.layout.list_item_entry, null);
-				
-				final ImageView skillIcon = (ImageView) v.findViewById(R.id.list_skill_icon);
-				final TextView skillName = (TextView) v.findViewById(R.id.list_skill_title);
-				final TextView skillCost = (TextView) v.findViewById(R.id.list_skill_cost_text);
-				final TextView skillGenerates = (TextView) v.findViewById(R.id.list_skill_generates);
-				final TextView skillCooldown = (TextView) v.findViewById(R.id.list_skill_cooldown);
-				final TextView skillRequiredLevel = (TextView) v.findViewById(R.id.list_skill_required_level);
-				final TextView skillDescription = (TextView) v.findViewById(R.id.list_skill_description);
-				
-				// Is this a terrible hack?! I think so...
-				int img = context.getResources().getIdentifier("drawable/" + s.getIcon(), null, context.getPackageName());
+                final TextView sectionView = (TextView) v.findViewById(R.id.list_item_section_text);
+                sectionView.setText(si.getTitle());
+            }
+            else 
+            {
+                Skill s = ((EntryItem) i).getSkill();
+                v = vi.inflate(R.layout.list_item_entry, null);
 
-				//@formatter:off
+                final ImageView skillIcon = (ImageView) v.findViewById(R.id.list_skill_icon);
+                final TextView skillName = (TextView) v.findViewById(R.id.list_skill_title);
+                final TextView skillCost = (TextView) v.findViewById(R.id.list_skill_cost_text);
+                final TextView skillGenerates = (TextView) v.findViewById(R.id.list_skill_generates);
+                final TextView skillCooldown = (TextView) v.findViewById(R.id.list_skill_cooldown);
+                final TextView skillRequiredLevel = (TextView) v.findViewById(R.id.list_skill_required_level);
+                final TextView skillDescription = (TextView) v.findViewById(R.id.list_skill_description);
+
+                // Is this a terrible hack?! I think so...
+                int img = context.getResources().getIdentifier("drawable/" + s.getIcon(), null, context.getPackageName());
+
+                //@formatter:off
 				skillIcon         .setImageResource(img);
 				skillName         .setText(s.getName());
 				skillCost         .setText("Cost: " + s.getCost());
@@ -67,10 +70,10 @@ public class EntryAdapter extends ArrayAdapter<Item> {
 				skillRequiredLevel.setText("Required Level: " + s.getRequiredLevel());
 				skillDescription  .setText(s.getDescription());
 				//@formatter:on
-				
-			}
-		}
-		return v;
-	}
+
+            }
+        }
+        return v;
+    }
 
 }
