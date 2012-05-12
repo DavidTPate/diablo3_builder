c23fecdd6488e9275f42a69c6f6c69a64baf21ed
davidtpate <davidtpate@gmail.com>
2012-05-11 20:58:29 -0400

    Add rune title, adjust layout slightly.

--- d3_android/src/com/pate/diablo/sectionlist/EntryRune.java
+++ d3_android/src/com/pate/diablo/sectionlist/EntryRune.java
@@ -10,60 +10,63 @@ import android.widget.TextView;
 
 import com.pate.diablo.R;
 import com.pate.diablo.model.Rune;
-import com.pate.diablo.model.Skill;
 
+public class EntryRune implements Item {
 
-public class EntryRune implements Item 
-{
+	private final Rune rune;
+	private final String skillName;
+	private final UUID skillUUID;
 
-    private final Rune rune;
-    private final String skillName;
-    private final UUID skillUUID;
-
-	public EntryRune(Rune rune, String skillName, UUID skillUUID) 
-	{
+	public EntryRune(Rune rune, String skillName, UUID skillUUID) {
 		this.rune = rune;
 		this.skillName = skillName;
 		this.skillUUID = skillUUID;
 	}
-	
-	public Rune getRune() 
-	{
-	    return rune;
+
+	public Rune getRune() {
+		return rune;
 	}
-	
-	public String getSkillName()
-	{
+
+	public String getSkillName() {
 		return skillName;
 	}
-	
-	public UUID getSkillUUID()
-	{
+
+	public UUID getSkillUUID() {
 		return skillUUID;
 	}
 
 	@Override
-    public View inflate(Context c, Item i) {
-        
-        LayoutInflater vi = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        
-        Rune r = ((EntryRune) i).getRune();
-        View v = vi.inflate(R.layout.list_item_rune, null);
+	public View inflate(Context c, Item i) {
 
-        final ImageView runeIcon = (ImageView) v.findViewById(R.id.list_rune_icon);
-        final TextView runeUnlockedAt = (TextView) v.findViewById(R.id.list_rune_unlocked_at);
-        final TextView runeDescription = (TextView) v.findViewById(R.id.list_rune_description);
-        
-        // Is this a terrible hack?! I think so...
-        int runeImage = c.getResources().getIdentifier("drawable/" + r.getIcon(), null, c.getPackageName());
+		LayoutInflater vi = (LayoutInflater) c
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        runeIcon.setBackgroundResource(runeImage);
-        runeUnlockedAt.setText((v.getContext().getString(R.string.Unlocked_at_level) + " " + r.getRequiredLevel()));
-        runeDescription.setText(r.getDescription());
-        
-        v.setTag(r.getUuid());
-        
-        return v;
-    }
-	
+		Rune r = ((EntryRune) i).getRune();
+		View v = vi.inflate(R.layout.list_item_rune, null);
+
+		final ImageView runeIcon = (ImageView) v
+				.findViewById(R.id.list_rune_icon);
+		final TextView runeTitle = (TextView) v
+				.findViewById(R.id.list_rune_title);
+		final TextView runeUnlockedAt = (TextView) v
+				.findViewById(R.id.list_rune_unlocked_at);
+		final TextView runeDescription = (TextView) v
+				.findViewById(R.id.list_rune_description);
+
+		// Is this a terrible hack?! I think so...
+		int runeImage = c.getResources().getIdentifier(
+				"drawable/" + r.getIcon(), null, c.getPackageName());
+
+		runeIcon.setBackgroundResource(runeImage);
+		runeTitle.setText(getRune().getName());
+		runeUnlockedAt.setText((v.getContext().getString(
+				R.string.Unlocked_at_level)
+				+ " " + r.getRequiredLevel()));
+		runeDescription.setText(r.getDescription());
+
+		v.setTag(r.getUuid());
+
+		return v;
+	}
+
 }
