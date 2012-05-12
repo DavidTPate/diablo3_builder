17582496e0e064a895ab05481c628d0a9a648b2d
davidtpate <davidtpate@gmail.com>
2012-05-11 21:55:50 -0400

    Update get and contains functions. Add new json.

--- d3_android/src/com/pate/diablo/model/Class.java
+++ d3_android/src/com/pate/diablo/model/Class.java
@@ -71,139 +71,145 @@ public class Class {
 	private String tier3Text;
 
 	private UUID uuid = UUID.randomUUID();
-
+	
 	public boolean containsActiveSkillByUUID(UUID uuid)
 	{
-	    return getActiveSkillByUUID(uuid) != null;
+		return containsSkillByUUID(activeSkills, uuid);
 	}
-
+	
 	public boolean containsActiveSkillsByRequiredLevel(int requiredLevel)
-	{	
-		return containsSkillsByRequiredLevel(getActiveSkills(), requiredLevel);
+	{
+		return containsSkillsByRequiredLevel(activeSkills, requiredLevel);
 	}
 	
 	public boolean containsActiveSkillsByType(String type)
 	{
-	    return ((List<Skill>) getActiveSkillsByType(type)).size() > 0;
+		return containsSkillsByType(activeSkills, type);
 	}
-
+	
 	public boolean containsPassiveSkillByUUID(UUID uuid)
-	{	
-	    return getPassiveSkillByUUID(uuid) != null;
+	{
+		return containsSkillByUUID(passiveSkills, uuid);
 	}
-
+	
 	public boolean containsPassiveSkillsByRequiredLevel(int requiredLevel)
-	{	
-		return containsSkillsByRequiredLevel(getPassiveSkills(), requiredLevel);
+	{
+		return containsSkillsByRequiredLevel(passiveSkills, requiredLevel);
 	}
-
+	
+	public boolean containsPassiveSkillsByType(String type)
+	{
+		return containsSkillsByType(passiveSkills, type);
+	}
+	
+	public boolean containsSkillByUUID(List<Skill> list, UUID uuid)
+	{
+		for (Skill skill : list)
+		{
+			if (skill.getUuid().equals(uuid))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public boolean containsSkillsByRequiredLevel(List<Skill> list, int requiredLevel)
 	{
-	    for (Skill skill : list)
+		for (Skill skill : list)
 		{
 			if (skill.getRequiredLevel() <= requiredLevel)
 			{
 				return true;
 			}
 		}
-		
 		return false;
 	}
 	
-	public boolean containsSkillsByType(String type)
+	public boolean containsSkillsByType(List<Skill> list, String type)
 	{
-		return containsActiveSkillsByType(type) || (type.equals("Passive") ? passiveSkills.size() > 0 ? true : false : false);
+		if (type.equals("Passive"))
+		{
+			return passiveSkills.size() > 0;
+		}
+		for (Skill skill : list)
+		{
+			if (skill.getType().equals(type))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public Skill getActiveSkillByUUID(UUID uuid)
-	{	
-	    for (Skill skill : getActiveSkills())
-		{
-			if (skill.getUuid().equals(uuid))
-			{
-				return skill;
-			}
-		}
-		return null;
+	{
+		return getSkillByUUID(activeSkills, uuid);
 	}
-
-	public List<Skill> getActiveSkills() {
-		return activeSkills;
-	}
-
+	
 	public List<Skill> getActiveSkillsByRequiredLevel(int requiredLevel)
 	{
-		return getSkillsByRequiredLevel(getActiveSkills(), requiredLevel);
+		return getSkillsByRequiredLevel(activeSkills, requiredLevel);
 	}
-
+	
 	public List<Skill> getActiveSkillsByType(String type)
 	{
-		ArrayList<Skill> skills = new ArrayList<Skill>();
-		
-		for (Skill skill : getActiveSkills())
-		{
-			if (skill.getType().equalsIgnoreCase(type))
-			{
-				skills.add(skill);
-			}
-		}
-		
-		return skills;
+		return getSkillsByType(activeSkills, type);
 	}
 	
 	public String getDescription() {
 		return description;
 	}
-
+	
 	public String getEquipment() {
 		return equipment;
 	}
-
+	
 	public List<Feature> getKeyFeatures() {
 		return keyFeatures;
 	}
-
+	
 	public String getLore() {
 		return lore;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
+	
 	public Skill getPassiveSkillByUUID(UUID uuid)
 	{
-	    for (Skill skill : getPassiveSkills())
-		{
-			if (skill.getUuid().equals(uuid))
-			{
-				return skill;
-			}
-		}
-		return null;
+		return getSkillByUUID(passiveSkills, uuid);
 	}
+	
 
+	
 	public List<Skill> getPassiveSkills() {
 		return passiveSkills;
 	}
 
 	public List<Skill> getPassiveSkillsByRequiredLevel(int requiredLevel)
 	{
-		return getSkillsByRequiredLevel(getPassiveSkills(), requiredLevel);
+		return getSkillsByRequiredLevel(passiveSkills, requiredLevel);
+	}
+
+	public List<Skill> getPassiveSkillsByType(String type)
+	{
+		return getSkillsByType(passiveSkills, type);
 	}
 
 	public String getResourceText() {
 		return resourceText;
 	}
-	
+
 	public String getResourceType() {
 		return resourceType;
 	}
-	
+
 	public int getScreenshotCount() {
 		return screenshotCount;
 	}
-	
+
 	public String[] getScreenshots() {
 		return screenshots;
 	}
@@ -212,29 +218,105 @@ public class Class {
 		return shortDescription;
 	}
 	
-	public List<Skill> getSkillsByRequiredLevel(List<Skill> list, int requiredLevel)
+	public Skill getSkillByUUID(List<Skill> list, UUID uuid)
 	{
+		for (Skill skill : list)
+		{
+			if (skill.getUuid().equals(uuid))
+			{
+				return skill;
+			}
+		}
+		return null;
+	}
+	
+	public List<Skill> getActiveSkillsByTypeAndRequiredLevel(String type, int requiredLevel)
+	{
+		return getSkillsByTypeAndRequiredLevel(activeSkills, type, requiredLevel);
+	}
+	
+	public List<Skill> getPassiveSkillsByTypeAndRequiredLevel(String type, int requiredLevel)
+	{
+		return getSkillsByTypeAndRequiredLevel(passiveSkills, type, requiredLevel);
+	}
+	
+	public List<Skill> getSkillsByTypeAndRequiredLevel(List<Skill> list, String type, int requiredLevel)
+	{
+		if (type.equals("Passive"))
+		{
+			return getPassiveSkillsByRequiredLevel(requiredLevel);
+		}
+		
 		ArrayList<Skill> skills = new ArrayList<Skill>();
 		
 		for (Skill skill : list)
 		{
-		    if (skill.getRequiredLevel() <= requiredLevel)
-		    {
-		        skills.add(skill);
-		    }
+			if (skill.getType().equals(type) && skill.getRequiredLevel() <= requiredLevel)
+			{
+				skills.add(skill);
+			}
 		}
-
 		return skills;
 	}
 	
-	public List<Skill> getSkillsByRequiredLevel(String type, int requiredLevel)
+	public boolean containsActiveSkillsByTypeAndRequiredLevel(String type, int requiredLevel)
 	{
-	    return getSkillsByRequiredLevel(getSkillsByType(type), requiredLevel);
+		return containsSkillsByTypeAndRequiredLevel(activeSkills, type, requiredLevel);
 	}
 	
-	public List<Skill> getSkillsByType(String type)
+	public boolean containsPassiveSkillsByTypeAndRequiredLevel(String type, int requiredLevel)
 	{
-		return (type.equals("Passive") ? passiveSkills : getActiveSkillsByType(type));
+		return containsSkillsByTypeAndRequiredLevel(passiveSkills, type, requiredLevel);
+	}
+	
+	public boolean containsSkillsByTypeAndRequiredLevel(List<Skill> list, String type, int requiredLevel)
+	{
+		if (type.equals("Passive"))
+		{
+			return containsPassiveSkillsByType(type);
+		}
+		
+		for (Skill skill : list)
+		{
+			if (skill.getType().equals(type) && skill.getRequiredLevel() <= requiredLevel)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public List<Skill> getSkillsByRequiredLevel(List<Skill> list, int requiredLevel)
+	{
+		ArrayList<Skill> skills = new ArrayList<Skill>();
+		
+		for (Skill skill: list)
+		{
+			if (skill.getRequiredLevel() <= requiredLevel)
+			{
+				skills.add(skill);
+			}
+		}
+		return skills;
+	}
+	
+	public List<Skill> getSkillsByType(List<Skill> list, String type)
+	{
+		if (type.equals("Passive"))
+		{
+			return passiveSkills;
+		}
+		
+		ArrayList<Skill> skills = new ArrayList<Skill>();
+		
+		for (Skill skill : list)
+		{
+			if (skill.getType().equals(type))
+			{
+				skills.add(skill);
+			}
+		}
+		return skills;
 	}
 	
 	public String getTier1Text() {
